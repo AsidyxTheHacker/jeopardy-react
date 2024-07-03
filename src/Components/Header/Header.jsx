81ebb9938461d691b53a330d72db66f547fd8905
Adam <adammiller.2000nj@gmail.com>
2024-07-03 15:18:39 -0400

    added Components folder

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import logo from '../../assets/logo.png';
+import volumeOn from '../../assets/volume-on.svg';
+import volumeOff from '../../assets/volume-off.svg';
+import './Header.css';
+import { useState } from 'react';
+
+export default function Header() {
+
+    const [volume, setVolume] = useState(true);
+    console.log(volume)
+
+    function changeVolume() {
+        if(volume) {
+            setVolume(false)
+        } else {
+            setVolume(true)
+        }
+        console.log(volume)
+    }
+
+    return (
+        <div className="header">
+            <img
+                src={logo}
+                alt=""
+                className='logo' />
+            <img
+                src={volume === false ? volumeOff : volumeOn}
+                alt="volume setting"
+                className='volume'
+                onClick={() => changeVolume()} />
+        </div>
+    )
+};
