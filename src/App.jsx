4eaa9c7b782592bced74a32d035fffef3e201205
Adam <adammiller.2000nj@gmail.com>
2024-07-10 16:35:41 -0400

    changed body styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import Header from './Components/Header/Header.jsx';
+import Body from './Components/Body/Body.jsx';
 
 function App() {
 
   return (
     <>
       <Header />
+      <Body />
     </>
   )
-}
+};
 
 export default App;
